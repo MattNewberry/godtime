c84bd2265eb01078e9a16133278a7ec298161e4f
Matthew Newberry <mattnewberry@me.com>
2013-04-07 04:24:56 -0400

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GodTime::Application.config.secret_key_base = '97e2f074b405c64c3a9907f0368683a040ef8e6566f5490a1ca9153251605ebbf0f1ed86b8d9b76fff86fcdd7ab9b656db14770cb4cda9e1a0dc38a0dc52ddbd'
